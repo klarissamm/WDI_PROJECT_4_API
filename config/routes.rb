d966f3da8add4245069a064a688c974d2785acb4
klarissamm <klarissamunz@gmail.com>
2017-02-16 12:19:54 +0000

    Added register and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'register', to: 'auenthications#register'
+  post 'login', to: 'auenthications#login'
 end
