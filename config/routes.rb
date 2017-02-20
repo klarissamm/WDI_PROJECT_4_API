7df5e8c82a2e9d1978b8fbbc25c46d147c403373
klarissamm <klarissamunz@gmail.com>
2017-02-20 12:25:16 +0000

    added routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :periods
   post 'register', to: 'authentications#register'
   post 'login', to: 'authentications#login'
+  resources :periods
+  resources :users
 end
