8806d1a85d62465c09d0191a7243a90f755f4698
klarissamm <klarissamunz@gmail.com>
2017-02-23 11:49:23 +0000

    migrated new scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218130724) do
+ActiveRecord::Schema.define(version: 20170223111851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id", using: :btree
+  end
 
   create_table "periods", force: :cascade do |t|
     t.date     "date"
@@ -37,5 +47,6 @@ ActiveRecord::Schema.define(version: 20170218130724) do
     t.string   "image"
   end
 
+  add_foreign_key "contacts", "users"
   add_foreign_key "periods", "users"
 end
